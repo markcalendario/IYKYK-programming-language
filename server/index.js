f8e1c6b3bd440d0e2a10545c360662711ff4c8a0
markcalendario <markcalendario@gmail.com>
2023-12-26 16:31:52 +0800

    build: setup socket

--- server/index.js
+++ server/index.js
@@ -1,9 +1,19 @@
 import dotenv from "dotenv";
-import express from "express";
-const app = express();
+import { createServer } from "http";
+import { Server } from "socket.io";
 
+// ENV Configuration
 dotenv.config({ path: `./.env.${process.env.NODE_ENV}` });
 
-app.listen(process.env.PORT, () => {
-  console.log(`Example app listening on port ${process.env.PORT}`);
+const httpServer = createServer();
+const io = new Server(httpServer, {
+  cors: { origin: process.env.CLIENT_URL }
+});
+
+io.on("connection", (socket) => {
+  // ...
+});
+
+httpServer.listen(process.env.PORT, () => {
+  console.log("IYKYK is running on port", process.env.PORT);
 });
