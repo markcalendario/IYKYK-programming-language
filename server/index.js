5ec19f75c9218511d020b1fb4684ed8f318a4582
markcalendario <markcalendario@gmail.com>
2023-12-28 14:05:25 +0800

    fix: headers

--- server/index.js
+++ server/index.js
@@ -27,7 +27,7 @@ app.use((req, res, next) => {
   res.set({
     "Access-Control-Allow-Origin": process.env.CLIENT,
     "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
-    "Access-Control-Allow-Headers": "Content-Type",
+    "Access-Control-Allow-Headers": "Access-Control-Allow-Origin",
     "Access-Control-Allow-Credentials": true
   });
   next();
