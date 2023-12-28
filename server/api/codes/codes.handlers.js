7666c6a071aab14ef3275049ad502356ebf134cf
markcalendario <markcalendario@gmail.com>
2023-12-28 12:55:35 +0800

    feat: error handling

--- server/api/codes/codes.handlers.js
+++ server/api/codes/codes.handlers.js
@@ -3,6 +3,13 @@ import { currentDir } from "../../sockets/sockets.utils.js";
 
 export function handleDownloadSessionCode(req, res) {
   const curDir = currentDir(import.meta.url);
-  const ykFile = path.join(curDir, `../../sessions/${req.params.sessionId}.yk`);
-  res.download(ykFile);
+  try {
+    const ykFile = path.join(
+      curDir,
+      `../../sessions/${req.params.sessionId}.yk`
+    );
+    res.download(ykFile);
+  } catch (error) {
+    res.status(500).send({ success: false, message: error.message });
+  }
 }
